4df99dc70059e2b392de0d8607043010fd35518e
JasonEtco <jasonetco@gmail.com>
2016-03-22 15:02:40 -0400

    Interactivity scripts - Wrote eventlistener for global navigation - Wrote eventlistener for homepage's Abstract modal

---
+++ public/assets/js/_js/scripts.js
@@ -0,0 +1,32 @@
+// GLOBAL VARIABLES
+var body = document.body;
+
+var navButton = document.getElementById('js--nav-button');
+var navClose  = document.getElementById('js--nav-close');
+var nav       = document.getElementById('js--global-nav');
+
+navButton.addEventListener('click', function() {
+	nav.classList.add('nav--open');
+});
+
+navClose.addEventListener('click', function() {
+	nav.classList.remove('nav--open');
+});
+
+
+
+// Abstract Modal
+
+var abstractModal = document.querySelector('.abstract-modal');
+var abstractModalButton = document.getElementById('js--abstract-button');
+var abstractModalClose = document.getElementById('js--abstract-close');
+
+abstractModalButton.addEventListener('click', function() {
+	abstractModal.classList.add('abstract-modal--open');
+	body.classList.add('body--no-scroll');
+});
+
+abstractModalClose.addEventListener('click', function() {
+	abstractModal.classList.remove('abstract-modal--open');
+	body.classList.remove('body--no-scroll');
+});
