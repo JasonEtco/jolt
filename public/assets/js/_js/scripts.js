d72d14540d0aff5f140d527ac4e0b4563f999663
JasonEtco <jasonetco@gmail.com>
2016-03-23 18:58:42 -0400

    Added smoothstate and relevant animations

--- public/assets/js/_js/scripts.js
+++ public/assets/js/_js/scripts.js
@@ -1,32 +1,34 @@
-// GLOBAL VARIABLES
-var body = document.body;
+start = function() {
+	// GLOBAL VARIABLES
+	var body = document.body;
 
-var navButton = document.getElementById('js--nav-button');
-var navClose  = document.getElementById('js--nav-close');
-var nav       = document.getElementById('js--global-nav');
+	var navButton = document.getElementById('js--nav-button');
+	var navClose  = document.getElementById('js--nav-close');
+	var nav       = document.getElementById('js--global-nav');
 
-navButton.addEventListener('click', function() {
-	nav.classList.add('nav--open');
-});
+	navButton.addEventListener('click', function() {
+		nav.classList.add('nav--open');
+	});
 
-navClose.addEventListener('click', function() {
-	nav.classList.remove('nav--open');
-});
+	navClose.addEventListener('click', function() {
+		nav.classList.remove('nav--open');
+	});
 
 
 
-// Abstract Modal
+	// Abstract Modal
 
-var abstractModal = document.querySelector('.abstract-modal');
-var abstractModalButton = document.getElementById('js--abstract-button');
-var abstractModalClose = document.getElementById('js--abstract-close');
+	var abstractModal = document.querySelector('.abstract-modal');
+	var abstractModalButton = document.getElementById('js--abstract-button');
+	var abstractModalClose = document.getElementById('js--abstract-close');
 
-abstractModalButton.addEventListener('click', function() {
-	abstractModal.classList.add('abstract-modal--open');
-	body.classList.add('body--no-scroll');
-});
+	abstractModalButton.addEventListener('click', function() {
+		abstractModal.classList.add('abstract-modal--open');
+		body.classList.add('body--no-scroll');
+	});
 
-abstractModalClose.addEventListener('click', function() {
-	abstractModal.classList.remove('abstract-modal--open');
-	body.classList.remove('body--no-scroll');
-});
+	abstractModalClose.addEventListener('click', function() {
+		abstractModal.classList.remove('abstract-modal--open');
+		body.classList.remove('body--no-scroll');
+	});
+}
