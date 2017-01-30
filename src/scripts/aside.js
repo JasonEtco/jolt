f56dac4003e2267e70d5a70b96b0cadf1f2c1f10
JasonEtco <jasonetco@gmail.com>
2017-01-29 22:41:25 -0500

    :art: Add favicon, add redirects

--- src/scripts/aside.js
+++
@@ -1,5 +0,0 @@
-const collapseBtns = document.querySelectorAll('.aside__collapse-btn');
-
-Array.from(collapseBtns).forEach(btn => btn.addEventListener('click', (e) => {
-  e.target.nextElementSibling.classList.toggle('is-visible');
-}));
